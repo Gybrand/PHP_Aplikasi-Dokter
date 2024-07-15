b8bb808720849e9aa1e2e1a72bcf62e675c032cc
Fikli Al Fa'iq Gyonanda <75402214+Gybrand@users.noreply.github.com>
2024-07-15 10:07:43 +0700

    Final Release

---
+++ application/views/home.php
@@ -0,0 +1,33 @@
+<div class="container">
+  <div class="row">
+    <br>
+    <div class="col-md-12">
+      <div class="well">
+        <div class="media">
+          <a class="pull-left">
+            <!-- <img src="<?php echo base_url(); ?>assets/img/logo/idi.png" class="media-object" style="width:64px;" alt="" /> -->
+          </a>
+          <div class="media-body">
+            <h2 class="media-heading" align="center">Selamat datang, <?php echo $this->session->userdata('namalengkap'); ?></h2>
+            <br>
+            <!-- <center>
+              <p>Repost by <a href='' title='' target='_blank'></a></p>
+            </center> -->
+
+            <h2 align="center">Sistem Informasi</h2>
+            <h2 align="center">Praktik Umum Dokter Hariati Salim</h2>
+            <h4 align="center">Villa Regency Tangerang II Blok FC 1/42 A</h4>
+            <h4 align="center">Telp. (021) 2235 9430 WA. 0815 8388 178</h4>
+          </div>
+        </div>
+      </div>
+      <!-- <blockquote>
+              <h2>
+                <p align="center">Selamat datang, <?php echo $this->session->userdata('namalengkap'); ?></p> 
+                <p align="center">SISTEM INFORMASI PRAKTIK DOKTER  </p>
+                <p align="center">dr. Hj. Siti Sundari, Sp.M,. M.kes  </p>
+              </h2>
+            </blockquote>-->
+    </div>
+  </div>
+</div>
